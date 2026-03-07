03b8af79a86794de553c891093ace2392b0651f9
Berke Kiran <berkekiranofficial@hotmail.com>
2026-03-08 02:50:09 +0300

    fix: remove hardcoded test key from DeploySwapRouter

--- script/DeploySwapRouter.s.sol
+++ script/DeploySwapRouter.s.sol
@@ -6,7 +6,7 @@ import "../src/SwapRouter.sol";
 
 contract DeploySwapRouter is Script {
     function run() external {
-        uint256 pk = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        uint256 pk = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(pk);
         address poolManager = 0xE03A1074c86CFeDd5C142C4F04F1a1536e203543;
 
